5f6bbf6e53f3705d64018a474edbaca9fa954c04
amanattar <attar.aman29@gmail.com>
2023-05-29 20:58:23 -0500

    created setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "textSummarizer"
+AUTHOR_USER_NAME = "amanattar"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "attar.aman29@gmail.com"
